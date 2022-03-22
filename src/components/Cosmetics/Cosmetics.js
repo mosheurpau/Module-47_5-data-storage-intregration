706091eae51bb0aecbae74d1a09826a6aaf0760e
mosheurpau <mosheur.pau@gamil.com>
2022-03-22 10:43:52 +0600

    Event handler with parameters

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { add } from '../utilities/calculate';
-// import add from '../utilities/calculate';
+import React, { useEffect, useState } from 'react';
+import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 66;
-    const total = add(first, second);
+    const [cosmetics, setCosmetics] = useState([]);
+
+    useEffect( () => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setCosmetics(data))
+    }, [])
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
-            <p>Total: {total}</p>
+            {
+                cosmetics.map(cosmetic => <Cosmetic     
+                    key={cosmetic.id}
+                    cosmetic = {cosmetic}
+                ></Cosmetic>)
+            }
         </div>
     );
 };
